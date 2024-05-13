930b6896f89ec189c2289ec118be416892750fc9
hironmoy-ray <hpray35@gmail.com>
2024-05-13 14:02:09 +0600

    TS Basics

--- output/script.js
+++ output/script.js
@@ -1,2 +1,19 @@
 "use strict";
 console.log("Hello World");
+let playerName;
+console.log(typeof playerName);
+playerName = "Hironmoy";
+playerName = 23;
+console.log(typeof playerName);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(4, 4));
+let fruit = ["apple", "banana"];
+fruit.push("12");
+const person = {
+    name: "Hironmoy",
+    age: 23,
+    captain: true,
+};
+person.age = 24;
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,26 @@
 console.log("Hello World");
+
+let playerName;
+console.log(typeof playerName);
+
+playerName = "Hironmoy";
+playerName=23;
+
+console.log(typeof playerName);
+
+function multiply(a:number,b:number){
+    return a*b;
+}
+
+console.log(multiply(4,4));
+
+let fruit = ["apple","banana"];
+fruit.push("12")
+
+const person={
+    name:"Hironmoy",
+    age: 23,
+    captain: true,
+}
+
+person.age=24;
